caaac9aad76272fd8292716d1ad733c10a068f3e
Daniel Dugas <daniel@dugas.ch>
2024-08-11 17:51:11 +0200

    tested play files for g1 walk, run and facedown slow

---
+++ src/play_g1_run_polar_breeze.py
@@ -0,0 +1,37 @@
+from extracted_policy import ExtractedPolicy
+from deepmimic_env import DPEnv
+import numpy as np
+import os
+from stable_baselines3 import PPO
+import torch as th
+
+if __name__ == "__main__":
+    """ POLAR BREEZE 65 """
+    env = DPEnv(motion="run", robot="unitree_g1")
+    """ 
+        self.MAX_EP_LENGTH = 1000
+        self.VEL_OBS_SCALE = 0.1
+        self.FRC_OBS_SCALE = 0.001
+        self.ADD_FOOT_CONTACT_OBS = True
+        self.ADD_TORSO_OBS = True
+        self.ADD_JOINT_FORCE_OBS = False
+        self.ADD_ABSPOS_OBS = False
+        self.ADD_PHASE_OBS = True
+        version = "v0.9HRS.no_hands_20xact_mocapscale0.85_rplim60"
+    """
+    chkpt_path = os.path.expanduser("~/deep_mimic/run_polar-breeze-65_videos/run_polar-breeze-65_best")
+    # chkpt_path = os.path.expanduser("~/deep_mimic/walk_woven-glade-55_videos/walk_woven-glade-55_best")
+    model = PPO.load(chkpt_path)
+    obs = env.reset()
+    obs = env.reset_model(idx_init=0)
+    ep_rew = 0
+    for i in range(1000):
+        env.render(mode="human")
+        obs_th = th.tensor(obs[None, :], dtype=th.float32)
+        a_th, _, _ = model.policy.forward(obs_th, deterministic=True)
+        a = a_th.detach().numpy()[0]
+        obs, reward, done, info = env.step(a)
+        ep_rew += reward
+        if done:
+            break
+    print("Episode reward: ", ep_rew)
